ca8aff6a9f853a312b6880f0f9a9a20660cfc4e7
Rodrigo_Zarate <rodrigo@thelucky.app>
2023-07-07 11:35:46 -0400

    first commit

---
+++ Inventory/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace Inventory
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> productList = new List<Product>();
+            productList.Add(new Product(){Name = "ProductA", Price = 100, Stock = 5});
+            productList.Add(new Product(){Name = "ProductB", Price = 200, Stock = 3});
+            productList.Add(new Product(){Name = "ProductC", Price = 50, Stock = 510});
+
+            Console.WriteLine("---- Actual List ----");
+            Inventory.PrintList(productList);
+            
+            var sortedList = Inventory.GetSortedList(productList, "Stock", false);
+
+            Console.WriteLine("---- Result List ----");    
+            Inventory.PrintList(sortedList);
+
+
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+    }
+
+    public static class Inventory
+    {
+        public static List<Product> GetSortedList(List<Product> productList, string sortKey, bool ascending = true)
+        {
+            var propertyInfo = typeof(Product).GetProperty(sortKey);
+            var sortedListByKey = ascending ? productList.OrderBy(p => propertyInfo.GetValue(p, null)).ToList()
+                                            : productList.OrderByDescending(p => propertyInfo.GetValue(p, null)).ToList();
+            return sortedListByKey;
+        }
+
+        public static void PrintList(List<Product> productList)
+        {
+            foreach (var item in productList)
+            {
+                Console.WriteLine("Name: " + item.Name + " Price: " + item.Price + " Stock: " + item.Stock);
+            }
+            
+        }
+    } 
+}
---
+++ MyLogger/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace MyLogger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new LoggerManager();
+            
+            // NOTE: Path for file log was not implement as paremeter in function because as a best practice at moment to deploy projects or automation 
+            // frameworks we should define where all utilities and files(reports, logs, etc) should be saved to avoid problems or exceptions when code or 
+            // automated tests are run on differents envs or deployed with pipelines on testing envs in CI/CT.
+
+            logger.Information("Clicking on Save button", "INFO"); // Acceptance test
+            logger.Information("Erron on Save button", "WARNING"); // Acceptance test
+            logger.Information("Clicking on Generate Button", "INFO"); // Functional test
+            logger.Information("Warning at moment to save file", "WARNING"); // Functional test
+            logger.Information("Erron at moment to save file", "ERROR"); // Functional test
+            logger.Information("", "WARNING"); // Functional test
+            logger.Information("", ""); // Functional test
+            logger.Information("Erron at moment to save file", ""); // Functional test
+            logger.Information("Erron at moment to save file", string.Empty); // Functional test
+            logger.Information(string.Empty, "ERROR"); // Functional test
+            logger.Information(11111111112222222222222, "INFO"); // Negative test for Message Parameter
+            logger.Information("Clicking Exit button", ERROR); // Negative test for Level Parameter
+            logger.Information(11111111112222222222222, 4564); // Negative test
+            logger.Information(12.5, 4.5); // Negative test for parameters
+            logger.Information("Clicking Exit button", false); // Negative test for Level Parameter
+            logger.Information("Clicking Exit button", true); // Negative test for Level Parameter
+            logger.Information(true, "ERROR"); // Negative test for Message Parameter
+            logger.Information(false, "ERROR"); // Negative test for Message Parameter
+            logger.Information(true, false); // Negative test for parameters
+            logger.Information(true, true); // Negative test for parameters
+            logger.Information(false, true); // Negative test for parameters
+            logger.Information(false, false); // Negative test for parameters
+            logger.Information("Clicking ###^^^%%&^*(&*(&$#))", "INFO"); // Negative test for parameters
+            logger.Information("Clicking exit button", "WARNING $$$$$$%^^^&&&***(())**"); // Negative test for parameters
+            logger.Information("1231547894651324658798787987987897", "INFO"); // Testing Boundary for Message Parameter
+            logger.Information("Click Button", "WARNINGGGGGGGGGGGGGGGG"); // Testing Boundary for Level Parameter
+
+        }
+    }
+
+    public sealed class LoggerManager
+    {
+        private static string LoggerReportPath = Directory.GetCurrentDirectory();
+        const string loggerFileName = @"application.log";
+        string fullPath = Path.Combine(LoggerReportPath, loggerFileName);
+
+        public LoggerManager()
+        {
+            if (!File.Exists(fullPath))
+            {
+                FileStream fs = new FileStream(fullPath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                fs.Close();
+            }
+        }
+
+        public void Information(string message, string level)
+        {     
+            var date = DateTime.Now.ToString("yyyy'-'MM'-'dd' 'HH':'mm':'ss");
+            string customTemplate = $"[{date}][{level}] {message}";
+
+            using(StreamWriter sw = File.AppendText(fullPath))
+            {
+                sw.WriteLine(customTemplate);
+                sw.Flush();
+                sw.Close();
+            }            
+        }
+
+        private static LoggerManager _logger;
+        public static LoggerManager Instance => _logger ?? (_logger = new LoggerManager());
+    } 
+}
